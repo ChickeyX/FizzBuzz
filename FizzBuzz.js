d8cc2b7a6c1c0a4a579b5ce5455b2e286bd0273c
Catherine H <59094124+ChickeyX@users.noreply.github.com>
2019-12-24 17:30:21 +0100

    Update FizzBuzz.js

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,6 +1,6 @@
-for (i = 1; i <= 100; i++) {              // Increment 'x' by 1 in each repeat (up to 100)
-  string = "";                            // Reset 'string'
-  if (i % 3 == 0) {string += "Fizz"}      // If 'i' is divisible by 3 then add "Fizz" to 'string'
-  if (i % 5 == 0) {string += "Buzz"}      // If 'i' is divisible by 5 then add "Buzz" to 'string'
-  console.log(string || i)                // If 'string' is empty, print 'i'. Otherwise, print 'string'
+for (i = 1; i <= 100; i++) {
+  string = ""
+  if (i % 3 == 0) {string += "Fizz"}
+  if (i % 5 == 0) {string += "Buzz"}
+  console.log(string || i)
 }
