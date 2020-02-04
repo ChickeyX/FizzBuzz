61777533fed401e533a7e3ac09e435607fa66155
Catherine H <59094124+ChickeyX@users.noreply.github.com>
2020-02-04 19:48:05 +0000

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,7 +1,7 @@
-for x in range(1, 101):
+for i in range(1, 101):
     response = ''
-    if x % 3 == 0:
+    if i % 3 == 0:
         response += 'Fizz'
-    if x % 5 == 0:
+    if i % 5 == 0:
         response += 'Buzz'
-    print(response or x)
+    print(response or i)
